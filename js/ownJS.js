f1e6ed8f4623552a59eda47c4acd1961fa74e755
Bram <bram.de.jaegher@gmail.com>
2016-12-27 15:17:33 +0100

    navbar content is up and running!

--- js/ownJS.js
+++ js/ownJS.js
@@ -2,8 +2,11 @@
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.paddingRight = "15px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.paddingRight = "0";
+
 }
