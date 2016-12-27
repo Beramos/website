c3cf7bb6153dfa0cf532e146a4bcc485d6dfd5d1
Bram <bram.de.jaegher@gmail.com>
2016-12-27 15:31:08 +0100

    sidebar mechanix

--- js/ownJS.js
+++ js/ownJS.js
@@ -2,11 +2,16 @@
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("mySidenav").style.paddingRight = "15px";
+    document.getElementById("mySidenav").style.paddingRight = "15px";
+    document.getElementById("mySidenav").style.borderStyle = "solid";
+    document.getElementById("mySidenav").style.borderRight = "thick double #545454";
+    document.getElementById("mySidenav").style.borderTop = "thick double #545454";
+    document.getElementById("mySidenav").style.borderBottom = "thick double #545454";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("mySidenav").style.paddingRight = "0";
+    document.getElementById("mySidenav").style.borderStyle = "none";
 
 }
