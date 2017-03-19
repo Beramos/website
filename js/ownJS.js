5eb66c7716ecdbe39120a7e25e6d423809d14262
Bram <bram.dejaegher@ugent.be>
2017-03-19 18:14:10 +0100

    fading background on list slideDown

--- js/ownJS.js
+++ js/ownJS.js
@@ -2,37 +2,83 @@
 
 /* Sidebar */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.paddingRight = "15px";
-    document.getElementById("mySidenav").style.borderStyle = "solid";
-    document.getElementById("mySidenav").style.borderRight = "thick double #545454";
-    document.getElementById("mySidenav").style.borderTop = "thick double #545454";
-    document.getElementById("mySidenav").style.borderBottom = "thick double #545454";
+    document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('mySidenav').style.paddingRight = '15px';
+    document.getElementById('mySidenav').style.borderStyle = 'solid';
+    document.getElementById('mySidenav').style.borderRight = 'thick double #545454';
+    document.getElementById('mySidenav').style.borderTop = 'thick double #545454';
+    document.getElementById('mySidenav').style.borderBottom = 'thick double #545454';
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mySidenav").style.paddingRight = "0";
-    document.getElementById("mySidenav").style.borderStyle = "none";
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('mySidenav').style.paddingRight = '0';
+    document.getElementById('mySidenav').style.borderStyle = 'none';
 
 }
 
 /* JQuery */
 
 $(document).ready(function() {
-  $(".hoverAction").hover(
-      function() { $(this).addClass("Hover"); 
+  $('.hoverAction').hover(
+      function() { $(this).addClass('Hover'); 
                  }, function() {
-                     $(this).removeClass("Hover");
+                     $(this).removeClass('Hover');
       });
-  $(".hoverAction").click(
+  $('.hoverAction').click(
     function(){
-        if ($(this).next().is(":visible")) {
-          $(this).next().slideUp(600);  
-        } 
-
-        else { 
-          $(this).next().slideDown(600);
-        }
+        $(this).css({
+            position: 'relative',
+            top     : 0,
+            left    : 0,
+            zIndex  : 99,
+            background : '#f2f2f2'
+        });
+        
+        $('.additionInfo').css({
+            position: 'relative',
+            top     : 0,
+            left    : 0,
+            zIndex  : 99,
+            background : '#f2f2f2'
+        });
+        
+        $(this).next().slideDown(600);  
+        
+        var w = $(document).width();
+        var h = $(document).height();
+        var $overlay = $('<div/>', {
+          'id': 'overlay',
+           css: {
+               position   : 'absolute',
+               height     : h + 'px',
+               width      : w + 'px',
+               left       : 0,
+               top        : 0,
+               background : '#000',
+               opacity    : 0.5,
+               zIndex     : 98
+          }
+         }).appendTo('body');
+        
+        var $topOverlay = $('<div/>', {
+          'id': 'topOverlay',
+           css: {
+               position   : 'absolute',
+               height     : h + 'px',
+               width      : w + 'px',
+               left       : 0,
+               top        : 0,
+               background : '#000',
+               opacity    : 0,
+               zIndex     : 100
+          }
+         }).appendTo('body');
+         
+         $('#topOverlay').click( function () {
+            $(this).remove();
+            $('#overlay').remove();
+            $('.additionInfo').slideUp(600);
+         })
     });
 });
